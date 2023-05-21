31f66ad5a68ef05eab2b1a0d2394d0fa49ae9877
rbzf <anna.sadkowska@gmail.com>
2023-05-21 09:51:27 +0200

    Rozwiazanie

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -2,4 +2,25 @@ package pl.javastart.task;
 
 public enum Season {
     //uzupełnij
+    SPRING("Wiosna", new String[]{"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[] {"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[] {"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[] {"grudzień", "styczeń", "luty"});
+
+    private final String plTranslation;
+    private final String[] monthList;
+
+    Season(String plTranslation, String[] monthList) {
+        this.plTranslation = plTranslation;
+        this.monthList = monthList;
+    }
+
+    public String getPlTranslation() {
+        return plTranslation;
+    }
+
+    public String[] getMonthList() {
+        return monthList;
+    }
+
 }
--- src/main/java/pl/javastart/task/SeasonManager.java
+++ src/main/java/pl/javastart/task/SeasonManager.java
@@ -1,12 +1,41 @@
 package pl.javastart.task;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SeasonManager {
 
     // uzupełnij metodę - do wczytywania danych użyj przekazany Scanner
     public void run(Scanner scanner) {
+        System.out.println("Podaj porę roku:");
+        printSeasons();
+        String userInput = scanner.nextLine();
+        Season season = getSeason(userInput);
+        if (season == null) {
+            System.out.println("Niewlasciwa nazwa pory roku");
+            return;
+        }
 
+        System.out.println("W tej porze roku są następujące miesiące:");
+        System.out.println(Arrays.toString(season.getMonthList()));
+
+    }
+
+    private static Season getSeason(String userInput) {
+        Season season = null;
+        for (int i = 0; i < Season.values().length; i++) {
+            if (userInput.equalsIgnoreCase(Season.values()[i].getPlTranslation())) {
+                season = Season.values()[i];
+                break;
+            }
+        }
+        return season;
+    }
+
+    private void printSeasons() {
+        for (Season season : Season.values()) {
+            System.out.println(season.getPlTranslation());
+        }
     }
 
 }
